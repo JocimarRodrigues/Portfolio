b9090a06ad9a6948f091b6d354982e7fc35f34a0
Jocimar Rodrigues Chagas <JocimarRodrigues1337@gmail.com>
2022-12-27 04:49:12 -0300

    Build 1.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,6 @@
-// Data
-document.querySelector('#horario')
 let hora = new Date();
-console.log(hora)
 
 document.querySelector('#horario h1')
 
 horario.innerHTML = `${hora.toLocaleString()}`
-
-let data = new Date()
-
-console.log(data.toGMTString())
 
