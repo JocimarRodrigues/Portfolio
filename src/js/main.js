5361d0809a90536d6a358e13308575d0187b038c
jocimar <jocimarrodrigues1337@gmail.com>
2022-12-31 13:32:45 -0300

    Alteração no js

--- src/js/main.js
+++ src/js/main.js
@@ -26,12 +26,12 @@ menuLinks.forEach((link) => {
 
 // Animação no nome
 
-function typeWriter(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i) => {
+function typeWriter(element) {
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = '';
+    textArray.forEach((letra, i) => {
         setTimeout(function() {
-            elemento.innerHTML += letra
+            element.innerHTML += letra
         }, 75 * i)
     })
     
